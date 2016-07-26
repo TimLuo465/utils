288300489d3d3b94a678ae8f51a0a3c475d86afa
YT00465 <Tim.Luo@augmentum.com.cn>
2016-07-26 16:36:10 +0800

    update some logic.

--- extensions/translate/translate.js
+++ extensions/translate/translate.js
@@ -1,87 +1,91 @@
 (function() {
-	function translate () {}
+    function translate() {}
 
-	HTMLElement.prototype.appendHTML = function(html) {
-		var div = document.createElement("div"),
-			fragment = document.createDocumentFragment(), nodes;
+    HTMLElement.prototype.appendHTML = function(html) {
+        var div = document.createElement("div"),
+            fragment = document.createDocumentFragment(),
+            nodes;
 
-		div.innerHTML = html;
-		nodes = div.childNodes;
-		for (var i = 0, l = nodes.length; i < l; i += 1) {
-			fragment.appendChild(nodes[i]);
-		};
-		this.appendChild(fragment);
+        div.innerHTML = html;
+        nodes = div.childNodes;
+        for (var i = 0, l = nodes.length; i < l; i += 1) {
+            fragment.appendChild(nodes[i]);
+        };
+        this.appendChild(fragment);
 
-		nodes = null;
-		fragment = null;
-	}
+        nodes = null;
+        fragment = null;
+    }
 
-	translate.prototype = {
-		prevTxt: "",
-		generateTransContanier: function() {
-			var container = "<div class='translate-yd'>" + 
-							"<div class='translate-container'><div class='translate-content'><span class='translate-text'></span><span class='translation'></span></div>" +
-							"<div class='tip'><em></em><ins></ins></div></div></div>";
+    translate.prototype = {
+        prevTxt: "",
+        generateTransContanier: function() {
+            var container = "<div class='translate-yd'><div class='translate-container'>" +
+                "<div class='translate-content'><span class='translate-text'></span><span class='translation'></span></div>" +
+                "<div class='tip'><em></em><ins></ins></div></div></div>";
 
-			document.body.appendHTML(container);
-		},
-		getSelectionTxt : function() {
-			if(document.selection) {
-				return document.selection.createRange().text.toString();
-			}
-			return document.getSelection().toString();
-			
-		},
-		getTranslate: function(selectionTxt, cb) {
-			var script = document.createElement("script");
-			script.src = encodeURI("http://fanyi.youdao.com/openapi.do?keyfrom=translatev1&key=1840651191&type=data&doctype=jsonp&version=1.1&only=translate&q=" + selectionTxt +"&callback=(" + cb + ")");
-			script.id = "translate-yd";
-			document.head.appendChild(script);
-		},
-		initEvent: function(container) {
-			var that = this;
+            document.body.appendHTML(container);
 
-			container.addEventListener("mouseup", function(e) {
-				var selectionTxt = that.getSelectionTxt(),
-					transContainer = document.querySelector(".translate-container"),
-					sh = window.pageOffset || document.documentElement.scrollTop || document.body.scrollTop;
+            return this;
+        },
+        getSelectionTxt: function() {
+            if (document.selection) {
+                return document.selection.createRange().text.toString();
+            }
+            return document.getSelection().toString();
 
-				if(!selectionTxt || (selectionTxt && that.prevTxt == selectionTxt) ) {
-					return that.hideTranslate();
-				}
-				
-				that.prevTxt = selectionTxt;
-				transContainer.style.top = e.clientY + 20 + sh + "px";
-				transContainer.style.left = e.clientX + 10 + "px";
+        },
+        getTranslate: function(selectionTxt, cb) {
+            var script = document.createElement("script");
+            script.src = encodeURI("http://fanyi.youdao.com/openapi.do?keyfrom=translatev1&key=1840651191&type=data&doctype=jsonp&version=1.1&only=translate&q=" + selectionTxt + "&callback=(" + cb + ")");
+            script.id = "translate-yd";
+            document.head.appendChild(script);
+        },
+        initEvent: function(container) {
+            var that = this;
 
-				that.getTranslate(selectionTxt, that.showTranslate);
-			});
-		},
-		hideTranslate: function() {
-			var that = this,
-				ts = document.querySelector("#translate-yd"),
-				transEle = document.querySelector(".translation"),
-				transContainer = document.querySelector(".translate-container");
+            container.addEventListener("mouseup", function(e) {
+                var selectionTxt = that.getSelectionTxt(),
+                    transContainer = document.querySelector(".translate-container"),
+                    sh = window.pageOffset || document.documentElement.scrollTop || document.body.scrollTop;
 
-			if(!ts) return;
+                if (!selectionTxt || (selectionTxt && that.prevTxt == selectionTxt)) {
+                    return that.hideTranslate();
+                }
 
-			that.prevTxt = "";
-			transContainer.style.display = "none";
-			transEle.textContent = "";
-			document.head.removeChild(ts);
-		},
-		showTranslate: function(data) {
-			if(!data) return;
-			var translation = data.translation,
-				transEle = document.querySelector(".translation"),
-				transContainer = document.querySelector(".translate-container");
+                that.prevTxt = selectionTxt;
+                transContainer.style.top = e.clientY + 20 + sh + "px";
+                transContainer.style.left = e.clientX + 10 + "px";
 
-			transContainer.style.display = "block";
-			transEle.textContent = translation;
-		}
-	}
+                that.getTranslate(selectionTxt, that.showTranslate);
+            });
+        },
+        hideTranslate: function() {
+            var that = this,
+                ts = document.querySelector("#translate-yd"),
+                transEle = document.querySelector(".translation"),
+                transContainer = document.querySelector(".translate-container");
 
-	var trans = new translate();
-	trans.generateTransContanier();
-	trans.initEvent(document.body);
+            if (!ts) return;
+
+            that.prevTxt = "";
+            transContainer.style.display = "none";
+            transEle.textContent = "";
+            document.head.removeChild(ts);
+        },
+        showTranslate: function(data) {
+            if (!data) return;
+            var translation = data.translation,
+                transEle = document.querySelector(".translation"),
+                transContainer = document.querySelector(".translate-container");
+
+            transContainer.style.display = "block";
+            transEle.textContent = translation;
+        },
+        init: function() {
+            this.generateTransContanier().initEvent(document.body);
+        }
+    }
+
+    new translate().init();
 })(window)
